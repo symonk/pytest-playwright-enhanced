b4e48fb93b93533fc515775e255b671bef164c9e
Simon K <jackofspaces@gmail.com>
2024-02-25 16:52:18 +0000

    fix race condition in CI for `test_plugin_is_registered`

--- tests/integration/test_stdout.py
+++ tests/integration/test_stdout.py
@@ -3,7 +3,7 @@ import pytest
 
 def test_plugin_is_registered(pytester: pytest.Pytester) -> None:
     result = pytester.runpytest()
-    result.stdout.fnmatch_lines(["*plugins: playwright-enhanced-0.1.2*"])
+    result.stdout.fnmatch_lines(["*plugins:*playwright-enhanced-0.1.2*"])
 
 
 def test_help_is_correct(pytester: pytest.Pytester) -> None:
