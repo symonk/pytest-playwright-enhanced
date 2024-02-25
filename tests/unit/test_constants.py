c546d71dd595dd762a32f9ac0afcd12d36c8c723
Simon K <jackofspaces@gmail.com>
2024-02-25 13:26:04 +0000

    rename tests after `enum` refactoring

--- tests/unit/test_constants.py
+++ tests/unit/test_constants.py
@@ -2,12 +2,12 @@ from pytest_playwright_enhanced.const import BrowserEngine
 from pytest_playwright_enhanced.const import FixtureScope
 
 
-def test_browser_engines_enums_are_str() -> None:
+def test_browser_engines_dataclass() -> None:
     assert FixtureScope.Function == "function"
     assert FixtureScope.Session == "session"
 
 
-def test_fixture_scopes_enums_are_str() -> None:
+def test_fixture_scopes_dataclass() -> None:
     assert BrowserEngine.CHROMIUM == "chromium"
     assert BrowserEngine.FIREFOX == "firefox"
     assert BrowserEngine.WEBKIT == "webkit"
