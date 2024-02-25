077fd7996c6e0897c7fcb898f4c084726a45a71d
Simon K <jackofspaces@gmail.com>
2024-02-25 13:35:46 +0000

    improve public `Api`, placeholder `PlaywrightPageMixin`

---
+++ tests/unit/test_pages.py
@@ -0,0 +1,8 @@
+from pytest_playwright_enhanced import PlaywrightPageMixin
+
+
+def test_playwright_page_mixin() -> None:
+    class Foo(PlaywrightPageMixin):
+        ...
+
+    assert isinstance(Foo(), PlaywrightPageMixin)
