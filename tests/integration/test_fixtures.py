fb0c33d9b99e3cca146ccfec32ce7dcee54a9f2a
Simon K <jackofspaces@gmail.com>
2024-03-03 07:53:28 +0000

    fix `test_sync_playwright_is_evailable`

--- tests/integration/test_fixtures.py
+++ tests/integration/test_fixtures.py
@@ -6,8 +6,8 @@ def test_sync_playwright_is_available(pytester: pytest.Pytester) -> None:
         """
         from playwright.sync_api import Playwright
 
-        def test_sync_playwright(playwright):
-            assert isinstance(playwright, Playwright)
+        def test_sync_playwright(pw_playwright):
+            assert isinstance(pw_playwright, Playwright)
 """,
     )
     result = pytester.runpytest()
