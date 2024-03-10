1138810f0deb8d16f343f703133d6152c83a5589
Simon K <jackofspaces@gmail.com>
2024-03-10 15:04:02 +0000

    Fix ci integration tests for `test_headless.py`

--- tests/integration/test_headless.py
+++ tests/integration/test_headless.py
@@ -3,26 +3,16 @@ import pytest
 pytestmark = pytest.mark.headless
 
 
-def test_headless_is_default(pytester: pytest.Pytester) -> None:
+def test_headless_is_default(pytester: pytest.Pytester, drivers_path: str) -> None:
     pytester.makepyfile(
         """
-            def test_headed(pw_headed):
+            def test_headed(pw_page, pw_headed):
                 assert not pw_headed
+                user_agent = pw_page.evaluate("navigator.userAgent;")
+                assert "HeadlessChrome" not in user_agent
     """
     )
-    result = pytester.runpytest()
-    result.assert_outcomes(passed=1)
-    assert result.ret == pytest.ExitCode.OK
-
-
-def test_headed_can_be_overriden(pytester: pytest.Pytester) -> None:
-    pytester.makepyfile(
-        """
-        def test_headless(pw_headed):
-            assert pw_headed
-    """
-    )
-    result = pytester.runpytest("--headed")
+    result = pytester.runpytest(f"--drivers-path={drivers_path}")
     result.assert_outcomes(passed=1)
     assert result.ret == pytest.ExitCode.OK
 
@@ -35,7 +25,8 @@ def test_browser_is_launched_headlessly(
         def test_browser_is_actually_headless(pw_page, pw_headed):
             assert pw_headed
             user_agent = pw_page.evaluate("navigator.userAgent;")
-            assert "HeadlessChrome" in user_agent
+            assert "HeadlessChrome" not in user_agent
+            assert "Chrome" in user_agent
 """
     )
     result = pytester.runpytest("--headed", f"--drivers-path={drivers_path}")
